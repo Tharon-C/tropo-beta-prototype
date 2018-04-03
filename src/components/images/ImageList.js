11f1b188ce5d1f8d75e050baaede29003d74f102
tharon-c <design@tharoncarlson.com>
2018-04-03 12:59:41 -0700

    fix image pagination

--- src/components/images/ImageList.js
+++ src/components/images/ImageList.js
@@ -1,35 +1,46 @@
-import React from "react";
+import React, { Component } from "react";
 import { connect } from "react-redux";
 import { FlatButton } from "material-ui";
 import { MediaCardGroup, Element } from "../../cyverse-ui/";
 import ImageCard from "./ImageCard";
 
+class ImageList extends Component {
+  state = { page: 1 };
+  requestPage = () => {
+    this.setState(state => {
+      return {
+        page: state.page + 1
+      };
+    });
+  };
 
-const ImageList = ({ filter = () => true, images, loadMoreEnteries, range }) => {
-  return (
-    <section style={{ maxWidth: "1000px", margin: "auto" }}>
-      <MediaCardGroup>
-      {images.filter(filter).slice(0,10).map((image, i) =>{
-          return (
-            <ImageCard key={image.id} image={image}/>
-          );
-        })}
-      </MediaCardGroup>
-      <FlatButton
-        style={{
-          marginTop: 22,
-          display: "block",
-          margin: "auto"
-        }}
-        primary
-        onClick={loadMoreEnteries}
-        label="Load More Images"
-      />
-    </section>
-  );
-};
-
+  render() {
+    const { filter = () => true, images, loadMoreEnteries, range } = this.props;
+    const currentList = images.filter(filter).slice(0, 20 * this.state.page);
+    return (
+      <section style={{ maxWidth: "1000px", margin: "auto" }}>
+        <MediaCardGroup>
+          {currentList.map((image, i) => {
+            return <ImageCard key={image.id} image={image} />;
+          })}
+        </MediaCardGroup>
+        {currentList.length >= 20 ? (
+          <FlatButton
+            style={{
+              marginTop: 22,
+              display: "block",
+              margin: "auto"
+            }}
+            primary
+            onClick={this.requestPage}
+            label="Load More Images"
+          />
+        ) : null}
+      </section>
+    );
+  }
+}
 const mapStateToProps = state => ({
   images: state.imageList.data
-})
+});
 export default connect(mapStateToProps, null)(ImageList);
