07dd28d0958fc5cce18ff7f04d3a878f1f61104a
tharon-c <design@tharoncarlson.com>
2018-04-09 12:51:11 -0700

    format sidebar

--- src/containers/SideBar.js
+++ src/containers/SideBar.js
@@ -2,7 +2,7 @@ import React from "react";
 import { push } from "react-router-redux";
 import { bindActionCreators } from "redux";
 import { connect } from "react-redux";
-import {zIndex} from "../styles/styles";
+import { zIndex } from "../styles/styles";
 import DashboardIcon from "material-ui/svg-icons/action/dashboard";
 import ListIcon from "material-ui/svg-icons/action/list";
 import NotificationsIcon from "material-ui/svg-icons/social/notifications";
@@ -21,7 +21,7 @@ const SideBar = ({
   volumes,
   projects,
   notifications,
-  current,
+  current
 }) => (
   <nav
     style={{
@@ -35,53 +35,52 @@ const SideBar = ({
       boxShadow: "1px 1px 3px 1px rgba(0,0,0,.3)"
     }}
   >
-  <SelectableList defaultValue={current || "dashboard"}>
-    <ListItem
-      value={"dashboard"}
-      onClick={dashboard}
-      primaryText="Dashboard"
-      leftIcon={<DashboardIcon />}
-    />
-    <ListItem
-    value={"image-catalog"}
-      onClick={imageCatalog}
-      primaryText="Image Catalog"
-      leftIcon={<ImageIcon />}
-    />
-    <ListItem
-    value={"all-assets"}
-      primaryText="All Assets"
-      onClick={allAssets}
-      leftIcon={<ListIcon />}
-      initiallyOpen
-    />
-    <ListItem
-    value={"instances"}
-      style={{ paddingLeft: "30px" }}
-      onClick={instances}
-      primaryText="Instances"
-      leftIcon={<InstanceIcon />}
-    />
-    <ListItem
-    value={"volumes"}
-      style={{ paddingLeft: "30px" }}
-      onClick={volumes}
-      primaryText="Volumes"
-      leftIcon={<VolumeIcon />}
-    />
-    <ListItem
-    value={"projects"}
-      style={{ paddingLeft: "30px" }}
-      onClick={projects}
-      primaryText="Projects"
-      leftIcon={<ProjectsIcon />}
-    />
-    <ListItem
-    value={"notifications"}
-      onClick={notifications}
-      primaryText="Notifications"
-      leftIcon={<NotificationsIcon />}
-    />
+    <SelectableList defaultValue={current || "dashboard"}>
+      <ListItem
+        value={"dashboard"}
+        onClick={dashboard}
+        primaryText="Dashboard"
+        leftIcon={<DashboardIcon />}
+      />
+      <ListItem
+        value={"image-catalog"}
+        onClick={imageCatalog}
+        primaryText="Image Catalog"
+        leftIcon={<ImageIcon />}
+      />
+      <ListItem
+        value={"all-assets"}
+        primaryText="All Assets"
+        onClick={allAssets}
+        leftIcon={<ListIcon />}
+      />
+      <ListItem
+        value={"instances"}
+        style={{ paddingLeft: "30px" }}
+        onClick={instances}
+        primaryText="Instances"
+        leftIcon={<InstanceIcon />}
+      />
+      <ListItem
+        value={"volumes"}
+        style={{ paddingLeft: "30px" }}
+        onClick={volumes}
+        primaryText="Volumes"
+        leftIcon={<VolumeIcon />}
+      />
+      <ListItem
+        value={"projects"}
+        style={{ paddingLeft: "30px" }}
+        onClick={projects}
+        primaryText="Projects"
+        leftIcon={<ProjectsIcon />}
+      />
+      <ListItem
+        value={"notifications"}
+        onClick={notifications}
+        primaryText="Notifications"
+        leftIcon={<NotificationsIcon />}
+      />
     </SelectableList>
   </nav>
 );
@@ -100,8 +99,8 @@ const mapDispatchToProps = dispatch =>
     dispatch
   );
 
-  const mapStateToProps = state => ({
-    current: state.routing.location.pathname.split("/")[1]
-  })
+const mapStateToProps = state => ({
+  current: state.routing.location.pathname.split("/")[1]
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(SideBar);
